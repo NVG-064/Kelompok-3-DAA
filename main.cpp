28b8630853101ede96085bd7ff4c979d1abc6389
ink0rr <tomflynnbeltsazar@gmail.com>
2021-06-22 19:43:35 +0700

    Delete film header

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h>
 #include <conio.h>
-#include "film.h"
 
 #define ARROW_UP 72
 #define ARROW_DOWN 80
@@ -10,6 +9,63 @@
 #define BACKSPACE 8
 
 using namespace std;
+
+class Film {
+	public:
+	int id;
+	string namaFilm;
+	int durasi;
+	string sinopsis;
+	vector< vector<bool> > kursi;
+
+	Film(){}
+	Film(int id, string namaFilm, int durasi, string sinopsis) : kursi(5, vector<bool> (5, false)){
+        this->id = id;
+		this->namaFilm = namaFilm;
+        this->durasi = durasi;
+        this->sinopsis = sinopsis;
+	}
+	print(){
+		cout << "ID: " << this->id << endl;
+        cout << "Nama Film: " << this->namaFilm << endl;
+        cout << "Durasi: " << this->durasi << endl;
+        cout << "Sinopsis: " << this->sinopsis << endl;
+	}
+    printKursi(){
+        for(int i = 0; i < 5; i++){
+			for(int j = 0; j < 5; j++){
+				cout << "["<< (char)( this->kursi[i][j] == true ? 'X' : i+'A' ) << (char)( this->kursi[i][j] == true ? 'X' : j+'1' ) << "] ";
+			}
+            cout << endl;
+		}
+    }
+    printKursi(int index1, int index2){
+        for(int i = 0; i < 5; i++){
+			for(int j = 0; j < 5; j++){
+				cout << ( i == index1 && j == index2 ? "[" : " ") << (char)( this->kursi[i][j] == true ? 'X' : i+'A' ) << (char)( this->kursi[i][j] == true ? 'X' : j+'1' ) << ( i == index1 && j == index2 ? "]" : " ");
+			}
+            cout << endl;
+		}
+    }
+};
+
+template <typename V>
+void printAllFilm(V& v) {
+    for (int i = 0; i < v.size(); i++) {
+        cout << endl;
+        v.at(i).print();
+        cout << endl;
+    }
+}
+template <typename V>
+void printAllFilm(V& v, int index) {
+    for (int i = 0; i < v.size(); i++) {
+        cout << ( index == i ? "\\/ \\/ \\/ \\/ \\/" : "" ) << endl;
+        v.at(i).print();
+        cout << ( index == i ? "/\\ /\\ /\\ /\\ /\\" : "" ) << endl;
+    }
+    cout << '\n';
+}
 
 int c;
 vector<Film> vFilm;
