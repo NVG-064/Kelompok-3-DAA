d3a0aa70aa69480ba80585ddf4be487b566fd96e
ink0rr <tomflynnbeltsazar@gmail.com>
2021-06-25 05:39:38 +0700

    Finish buy ticket feature

--- main.cpp
+++ main.cpp
@@ -10,107 +10,179 @@
 
 using namespace std;
 
-class Film {
+string getSeatNumber(int index1, int index2, bool flag){
+	if(flag){
+		return "XX";
+	}
+	char a = index1+'A';
+	char b = index2+'1';
+	return string() + a + b;
+}
+
+class JadwalFilm {
 	public:
-	int id;
-	string namaFilm;
-	int durasi;
-	string sinopsis;
+	string jamTayang;
 	vector< vector<bool> > kursi;
 
-	Film(){}
-	Film(int id, string namaFilm, int durasi, string sinopsis) : kursi(5, vector<bool> (5, false)){
-        this->id = id;
-		this->namaFilm = namaFilm;
-        this->durasi = durasi;
-        this->sinopsis = sinopsis;
+	JadwalFilm(){}
+	JadwalFilm(string jamTayang) : kursi(5, vector<bool> (5, false)){
+		this->jamTayang = jamTayang;
 	}
-	print(){
-		cout << "ID: " << this->id << endl;
-        cout << "Nama Film: " << this->namaFilm << endl;
-        cout << "Durasi: " << this->durasi << endl;
-        cout << "Sinopsis: " << this->sinopsis << endl;
-	}
-    printKursi(){
-        for(int i = 0; i < 5; i++){
-			for(int j = 0; j < 5; j++){
-				cout << "["<< (char)( this->kursi[i][j] == true ? 'X' : i+'A' ) << (char)( this->kursi[i][j] == true ? 'X' : j+'1' ) << "] ";
-			}
-            cout << endl;
-		}
-    }
     printKursi(int index1, int index2){
         for(int i = 0; i < 5; i++){
 			for(int j = 0; j < 5; j++){
-				cout << ( i == index1 && j == index2 ? "[" : " ") << (char)( this->kursi[i][j] == true ? 'X' : i+'A' ) << (char)( this->kursi[i][j] == true ? 'X' : j+'1' ) << ( i == index1 && j == index2 ? "]" : " ");
+				cout << ( i == index1 && j == index2 ? "[" : " ") << getSeatNumber(i, j, this->kursi[i][j]) << ( i == index1 && j == index2 ? "]" : " ");
 			}
             cout << endl;
 		}
     }
 };
 
-template <typename V>
-void printAllFilm(V& v) {
+class Film {
+	public:
+	int id;
+	string namaFilm;
+	string ratingUsia;
+	int durasi;
+	vector<JadwalFilm> jadwalFilm;
+
+	Film(){}
+	Film(int id, string namaFilm, string ratingUsia, int durasi, vector<JadwalFilm> jadwalFilm) {
+        this->id = id;
+		this->namaFilm = namaFilm;
+		this->ratingUsia = ratingUsia;
+        this->durasi = durasi;
+		this->jadwalFilm = jadwalFilm;
+	}
+	print(){
+		cout << "ID: " << this->id << endl;
+        cout << "Nama Film: " << this->namaFilm << endl;
+		cout << "Rating Usia: " << this->ratingUsia << endl;
+        cout << "Durasi: " << this->durasi << endl;
+	}
+	printJadwal(int index) {
+		for (int i = 0; i < jadwalFilm.size(); i++) {
+        	cout << ( index == i ? "[" : " " ) << i+1 << ( index == i ? "]" : " " ) << jadwalFilm.at(i).jamTayang << endl	;
+		}
+	}
+};
+
+class Pembelian {
+	public:
+	int id;
+	string namaFilm;
+	string jamTayang;
+	string kursiDibeli;
+	int totalHarga;
+	Pembelian(){}
+	print(){
+		cout << "ID Pembelian: " << this->id << endl;
+        cout << "Nama Film: " << this->namaFilm << endl;
+		cout << "Jam Tayang: " << this->jamTayang << endl;
+		cout << "Kursi dibeli: " << this->kursiDibeli << endl;
+        cout << "Total Harga: " << this->totalHarga << endl;
+	}
+};
+
+void printAllFilm(vector<Film> v, int index) {
     for (int i = 0; i < v.size(); i++) {
-        cout << endl;
+        cout << ( index == i ? "= = = = = = = = = = = = = = = = = = = =" : "" ) << endl;
         v.at(i).print();
-        cout << endl;
-    }
-}
-template <typename V>
-void printAllFilm(V& v, int index) {
-    for (int i = 0; i < v.size(); i++) {
-        cout << ( index == i ? "\\/ \\/ \\/ \\/ \\/" : "" ) << endl;
-        v.at(i).print();
-        cout << ( index == i ? "/\\ /\\ /\\ /\\ /\\" : "" ) << endl;
+        cout << ( index == i ? "= = = = = = = = = = = = = = = = = = = =" : "" ) << endl;
     }
     cout << '\n';
 }
 
-int c;
-vector<Film> vFilm;
 
-void pilihKursi(Film* pFilm) {
-	Film film = *pFilm;
+
+
+
+int c;
+int saldo = 0;
+vector<Film> vFilm;
+vector<Pembelian> vPembelian;
+Pembelian pembelian;
+
+// TODO: Refactor Code
+void pilihKursi(JadwalFilm* pJadwalFilm) {
+	JadwalFilm jadwalFilm = *pJadwalFilm;
+	pembelian.jamTayang = jadwalFilm.jamTayang;
 	int index1 = 0;
 	int index2 = 0;
-	int jumlahKursi = 0;
+	int totalHarga = 0;
+	string kursiDibeli = "";
 	for(;;){
 		system("cls");
-		film.printKursi(index1, index2);
+		jadwalFilm.printKursi(index1, index2);
 		cout << endl;
-		cout << ( index1 == film.kursi.size() ? "[Buy]" : " Buy " ) << endl;
-		cout << " Total: Rp. " << 50000 * jumlahKursi << endl;
+		cout << ( index1 == jadwalFilm.kursi.size() ? "[Buy]" : " Buy " ) << endl;
+		cout << " Total: Rp. " << totalHarga << endl;
 		switch(c = getch()){
 			case ARROW_UP:
 				if(index1-- == 0)
-					index1 = film.kursi.size();
+					index1 = jadwalFilm.kursi.size();
 				break;
 			case ARROW_DOWN:
-				if(index1++ == film.kursi.size())
+				if(index1++ == jadwalFilm.kursi.size())
 					index1 = 0;
 				break;
 			case ARROW_LEFT:
 				if(index2-- == 0)
-					index2 = film.kursi.at(0).size()-1;
+					index2 = jadwalFilm.kursi.at(0).size()-1;
 				break;
 			case ARROW_RIGHT:
-				if(index2++ == film.kursi.at(0).size()-1)
+				if(index2++ == jadwalFilm.kursi.at(0).size()-1)
 					index2 = 0;
 				break;
 			case BACKSPACE:
 				return;
 			case ENTER:
-				if(index1 != film.kursi.size()){
-					if(!film.kursi.at(index1).at(index2)){
-						film.kursi.at(index1).at(index2)= true;
-						jumlahKursi++;
+				if(index1 != jadwalFilm.kursi.size()){
+					if(!jadwalFilm.kursi[index1][index2]){
+						kursiDibeli += getSeatNumber(index1, index2, false) + " ";
+						jadwalFilm.kursi.at(index1).at(index2) = true;
+						totalHarga += 25000;
 					}
+				}
+				else if (index1 == jadwalFilm.kursi.size()){
+					pJadwalFilm->kursi = jadwalFilm.kursi;
+					pembelian.id = vPembelian.size()+1;
+					pembelian.kursiDibeli = kursiDibeli;
+					pembelian.totalHarga = totalHarga;
+
+					vPembelian.push_back(pembelian);
+					pembelian.print();
+					system("pause");
 				}
 				break;
 		}
 	}
 }
+// TODO: Refactor Code
+void pilihJadwal(Film* film){
+	pembelian.namaFilm = film->namaFilm;
+	int index = 0;
+	for(;;){
+		system("cls");
+		film->printJadwal(index);
+		switch(c = getch()){
+			case ARROW_UP:
+				if(index-- == 0)
+					index = film->jadwalFilm.size()-1;
+				break;
+			case ARROW_DOWN:
+				if(index++ == film->jadwalFilm.size()-1)
+					index = 0;
+				break;
+			case BACKSPACE:
+				return;
+			case ENTER:
+				pilihKursi(&film->jadwalFilm.at(index));
+				break;
+		}
+	}
+}
+
 void beliTiket() {
 	int index = 0;
 	for(;;){
@@ -128,7 +200,7 @@ void beliTiket() {
 			case BACKSPACE:
 				return;
 			case ENTER:
-				pilihKursi(&vFilm.at(index));
+				pilihJadwal(&vFilm.at(index));
 				break;
 		}
 	}
@@ -138,11 +210,10 @@ void mainMenu() {
 	int index = 1;
 	for(;;) {
 		system("cls");
-		cout << ( index == 1 ? "[1]" : " 1 " ) << "Cek Jadwal Film" << endl;
-		cout << ( index == 2 ? "[2]" : " 2 " ) << "Beli Tiket Film" << endl;
-		cout << ( index == 3 ? "[3]" : " 3 " ) << "Riwayat Pembelian" << endl;
-		cout << ( index == 4 ? "[4]" : " 4 " ) << "Top Up Saldo" << endl;
-		cout << ( index == 5 ? "[5]" : " 5 " ) << "Exit" << endl;
+		cout << ( index == 1 ? "[1]" : " 1 " ) << "Beli Tiket Film" << endl;
+		cout << ( index == 2 ? "[2]" : " 2 " ) << "Riwayat Pembelian" << endl;
+		cout << ( index == 3 ? "[3]" : " 3 " ) << "Top Up Saldo" << endl;
+		cout << ( index == 4 ? "[4]" : " 4 " ) << "Exit" << endl;
 		switch(c = getch()){
 			case ARROW_UP:
 				if(index-- == 1)
@@ -155,14 +226,9 @@ void mainMenu() {
 			case ENTER:
 				switch(index) {
 					case 1:
-						system("cls");
-						printAllFilm(vFilm);
-						system("pause");
-						break;
-					case 2:
 						beliTiket();
 						break;
-					case 5:
+					case 4:
 						return;
 				}
 				break;
@@ -171,9 +237,26 @@ void mainMenu() {
 }
 int main()
 {
-	string namaFilm[] = {"A", "B", "C"};
+	string namaFilm[] = {
+		"The Lord of the Rings",
+		"Inception",
+		"Interstellar",
+		"Joker",
+		"Up"
+	};
+	string jadwalFilm[] = {
+		"10:00",
+		"13:00",
+		"15:00",
+		"20:00"
+	};
 	for(int i = 0; i < sizeof(namaFilm)/sizeof(namaFilm[0]); i++){
-		Film film(vFilm.size()+1, namaFilm[i], 120, "bla");
+		vector<JadwalFilm> vJF;
+		for(int j = 0; j < sizeof(jadwalFilm)/sizeof(jadwalFilm[0]); j++){
+			JadwalFilm jf(jadwalFilm[j]);
+			vJF.push_back(jf);
+		}
+		Film film(vFilm.size()+1, namaFilm[i], "13+", 120, vJF);
 		vFilm.push_back(film);
 	}
 	mainMenu();
