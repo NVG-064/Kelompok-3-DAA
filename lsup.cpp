f16f13614fe7192acd0584ccdf0abac533449f2c
NVG-64 <aricandra064@student.esaunggul.ac.id>
2021-07-04 22:00:18 +0700

    Redesign Login and Sign Up Page

--- lsup.cpp
+++ lsup.cpp
@@ -3,6 +3,14 @@
 
 #include <iostream>
 #include <conio.h>
+
+#define ARROW_UP 72
+#define ARROW_DOWN 80
+#define ARROW_LEFT 75
+#define ARROW_RIGHT 77
+#define ENTER 13
+#define BACKSPACE 8
+
 using namespace std;
 
 struct dataPribadi{
@@ -11,9 +19,11 @@ struct dataPribadi{
     int curr;
 }d;
 
+int c;
+
 void signUp(){
     system("cls");
-    cin.ignore();
+    //cin.ignore();
     string nama, password;
 
     cout << "\n\tSIGN UP";
@@ -28,54 +38,82 @@ void signUp(){
 }
 
 void mainPage(){
-    int pil;
-    do {
+    int index = 1;
+    string nama, password;
+    for (;;){
         system("cls");
-        cout << "\n\tSELAMAT DATANG!\n\n1. Login\n2. Sign Up\n\nPilihan: ";
-        cin >> pil;
+        cout << "\n\tSELAMAT DATANG!\n\n";
+        cout << (index == 1 ? "[1]" : " 1 ") << " Login\n";
+        cout << (index == 2 ? "[2]" : " 2 ") << " Sign Up\n\n";
+        cout << (index == 3 ? "[3]" : " 3 ") << " Exit";
 
-        if (pil == 1){
-            system("cls");
-            cin.ignore();
-            string nama, password;
-
-            cout << "\n\n\tLOGIN";
-            cout << "\n\nNama: ";
-            getline(cin, nama);
-
-            cout << "Password: ";
-            getline(cin, password);
-
-            if ((nama == d.nama) && (password == d.password)){
-                cout << "\nLogin berhasil! Tekan apapun untuk melanjutkan.";
-                getch();
-                system("cls");
-                cout << "\nSelamat datang " << d.nama << ". kamu mendapatkan bonus 10 koin sebagai pengguna baru";
-                d.curr = 10;
-                getch();
-                break;
+        switch (c = getch()){
+            case ARROW_UP:
+            if (index-- == 1){
+                index = 3;
             }
+            break;
 
-            else if ((nama != d.nama) && (password == d.password)){
-                cout << "\nNama atau password salah!";
-                getch();
+            case ARROW_DOWN:
+            if (index++ == 3){
+                index = 1;
             }
+            break;
 
-            else if ((nama == d.nama) && (password != d.password)){
-                cout << "\nNama atau password salah!";
-                getch();
-            }
+            case ENTER:
+                switch (index){
 
-            else {
-                cout << "\nAkun tidak ditemukan! Silahkan sign up terlebih dahulu.";
-                getch();
-            }
+                    case 1:
+                    //cin.ignore();
+                    system("cls");
+
+                    cout << "\n\n\tLOGIN";
+                    cout << "\n\nNama: ";
+                    getline(cin, nama);
+
+                    cout << "Password: ";
+                    getline(cin, password);
+
+                    if ((nama == d.nama) && (password == d.password)){
+                        cout << "\nLogin berhasil! Tekan apapun untuk melanjutkan.";
+                        getch();
+                        system("cls");
+                        cout << "\nSelamat datang " << d.nama << ". kamu mendapatkan bonus 10 koin sebagai pengguna baru :) ";
+                        d.curr = 10;
+                        getch();
+                        break;
+                    }
+
+                    else if ((nama != d.nama) && (password == d.password)){
+                        cout << "\nNama atau password salah!";
+                        getch();
+                        break;
+                    }
+
+                    else if ((nama == d.nama) && (password != d.password)){
+                        cout << "\nNama atau password salah!";
+                        getch();
+                        break;
+                    }
+
+                    else {
+                        cout << "\nAkun tidak ditemukan! Kamu akan berpindah ke laman Sign Up.\nTekan apa saja untuk melanjutkan.";
+                        getch();
+                    }
+
+
+
+                    case 2:
+                    signUp();
+                    break;
+
+
+
+                    case 3:
+                    return;
+                }
         }
-
-        else if (pil == 2){
-            signUp();
-        }
-    } while ((pil > 0) && (pil < 3));
+    }
     
 }
 
